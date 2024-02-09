df7daebcdf6bff465437067cc6cce4157dcec5b7
Brandon Ward <brandon.ward1993@gmail.com>
2024-02-09 00:08:29 -0700

    Pathing and config options

--- builds/default.js
+++ builds/default.js
@@ -2,12 +2,15 @@ module.exports = {
     board_env: "STM32F103RE_creality",
     only: "stable",
     meta: {
-        stable_name: "ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
-        nightly_name: "ender_3_4.2.2-{{current_date}}-default-{{uid}}"
+        stable_name: "bdub_ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
+        nightly_name: "bdub_ender_3_4.2.2-{{current_date}}-default-{{uid}}"
     },
     based_on: {
         repo: "https://github.com/bwvolleyball/ender3v2-professional-firmware-configurations/",
-        path: "BW_Ender3V2_02062024",
-        stable_branch: "main"
-    }
+        path: "/BW_Ender3V2_02062024/",
+        stable_branch: "main",
+        nightly_branch: "main"
+    },
+    configuration: {},
+    configuration_adv: {}
 };
